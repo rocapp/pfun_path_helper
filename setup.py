c148cf0436d58d770d849bfd243b3ef6ca8250f4
Robbie Capps <rocapp@gmail.com>
2025-06-14 12:25:18 -0700

    removed old setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='path_helper',
-    description='Helper for appending the package path to sys.path',
-    version='0.0.3',
-    packages=['pfun_path_helper'],
-    install_requires=[]
-)
